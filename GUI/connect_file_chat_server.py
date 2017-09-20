358add80a323549d412396f1bb4a950ee400c2c9
FunkyPantss <1040359429@qq.com>
2017-09-20 16:06:31 +0800

    modify file and chat socket connect

--- GUI/connect_file_chat_server.py
+++ GUI/connect_file_chat_server.py
@@ -6,10 +6,27 @@ CHAT_PORT = 44444
 BUFFERSIZE = 1024
 CHAT_ADDR = (HOST, CHAT_PORT)
 
-FILE_ADDR = 44445
+FILE_PORT = 44445
+FILE_ADDR = (HOST, FILE_PORT)
 
-CONNECTED = False
+CHAT_CONNECTED = False
+FILE_CONNECTED = False
 
-while True:
+while CHAT_CONNECTED is False:
     try:
-        
+        chat_tcpCliSock = socket(AF_INET, SOCK_STREAM)
+        chat_tcpCliSock.connect(CHAT_ADDR)
+        CONNECTED = True
+        print('成功连接到聊天服务器')
+    except:
+        print('连接聊天服务器时出错')
+
+while FILE_CONNECTED is False:
+    try:
+        file_tcpCliSock = socket(AF_INET, SOCK_STREAM)
+        file_tcpCliSock.connect(FILE_ADDR)
+        CONNECTED = True
+        print('成功连接到文件服务器')
+    except:
+        print('连接文件服务器时出错')
+
